87ad3fb68953a1497be2368e003f7d9e28b64e27
Spiritvoin <sergey.korkishko@sigmalion.com.ua>
2017-07-24 15:25:46 +0300

    init

---
+++ frontend/keyword-planner/api/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spiritvoin
+ * Date: 24.07.17
+ * Time: 14:38
+ */
+
+
+require_once 'GetKeywordIdeas.php';
---
+++ frontend/keyword-planner/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spiritvoin
+ * Date: 24.07.17
+ * Time: 14:40
+ */
+echo 'dfd';
