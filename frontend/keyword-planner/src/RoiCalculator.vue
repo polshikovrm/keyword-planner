73af37aa0e0fa65fd1c6e54fab24bcb096dd1306
Sorokina Anna <anna.sorokina@sigmalion.com.ua>
2017-08-14 16:13:33 +0300

    #7500 calculator-roi page

--- frontend/keyword-planner/src/RoiCalculator.vue
+++ frontend/keyword-planner/src/RoiCalculator.vue
@@ -1,39 +1,56 @@
 <template>
     <div id="app">
         <div class="content-holder">
-            <h2>Step 2</h2>
-            <table class="table">
-                <thead>
+            <div class="calculate-holder">
+                <h2>Step 2</h2>
+                <table class="table">
+                    <thead>
                     <tr>
-                        <th>ROI Calculator</th>
+                        <th colspan="2">ROI Calculator</th>
                     </tr>
-                </thead>
-                <tbody>
+                    </thead>
+                    <tbody>
                     <tr v-for="(item, index) in planKeyword">
                         <td>{{item.keyword}}</td>
                         <td>{{item.searchVolume | formatNumber}}</td>
                     </tr>
-                    <tr>
-                        <td></td>
-                        <td><span class="total">Total</span>{{total}}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <form action="#" class="calculate-form">
-                <fieldset>
-                    <label for="conversion-rate">Conversion Rate</label>
-                    <input type="number" id="conversion-rate" placeholder="4.0">
-                    <label for="cpc">CPC</label>
-                    <div class="currency-box"><input type="number" id="cpc" placeholder="8.50"></div>
-                    <label for="for-client">Revenue For Client</label>
-                    <div class="currency-box"><input type="number" id="for-client" placeholder="2 000"></div>
-                    <div class="submit-row">
-                        <input type="submit" value="Calculate" class="btn yellow">
-                    </div>
-                </fieldset>
-            </form>
-            <div class="calculate-text">
-                <p>Based on the search demand and inputted metrics, you can expect a 177% return on investment from your ClientFlo Adwords campaign. This means for every $1 you invest, you should expect $1.77 in revenue.</p>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td></td>
+                            <td><span class="total">Total</span>{{total}}</td>
+                        </tr>
+                    </tfoot>
+                </table>
+                <form action="#" class="calculate-form">
+                    <fieldset>
+                        <div class="field">
+                            <label for="conversion-rate">Conversion Rate</label>
+                            <input type="text" id="conversion-rate" placeholder="4.0" class="width1">
+                        </div>
+                        <div class="field">
+                            <label for="cpc">CPC</label>
+                            <div class="currency-box">
+                                <span class="currency">$</span>
+                                <input type="text" id="cpc" placeholder="8.50"  class="width2">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label for="for-client">Revenue For Client</label>
+                            <div class="currency-box">
+                                <span class="currency">$</span>
+                                <input type="text" id="for-client" placeholder="2 000" class="width3">
+                            </div>
+                        </div>
+
+                        <div class="submit-row">
+                            <input type="submit" value="Calculate" class="btn">
+                        </div>
+                    </fieldset>
+                </form>
+                <div class="calculate-text">
+                    <p>Based on the search demand and inputted metrics, you can expect a 177% return on investment from your ClientFlo Adwords campaign. This means for every $1 you invest, you should expect $1.77 in revenue.</p>
+                </div>
             </div>
         </div>
     </div>
