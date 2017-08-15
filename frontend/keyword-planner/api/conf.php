2d6e80ca51adb6cff87db13c61b25ed028b37350
Spiritvoin <sergey.korkishko@sigmalion.com.ua>
2017-08-15 10:40:22 +0300

    add conf-local.php

--- frontend/keyword-planner/api/conf.php
+++ frontend/keyword-planner/api/conf.php
@@ -1,8 +1,14 @@
 <?php
 
-$GLOBALS['PDOconnect']=[
-    'host'=>'localhost',
-    'username'=>'root',
-    'password'=>'root',
-    'dbname'=>'keyword-planner',
-];
+$filename = __DIR__.'/conf-local.php';
+if (file_exists($filename)) {
+    require_once $filename;
+} else {
+    $GLOBALS['PDOconnect']=[
+        'host'=>'localhost',
+        'username'=>'root',
+        'password'=>'root',
+        'dbname'=>'keyword-planner',
+    ];
+}
+
